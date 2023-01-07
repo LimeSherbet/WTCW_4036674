fbc7af94832afdbdc0507fb01c4f55b4a2c1426a
John Gaskell <john.liam.gaskell@gmail.com>
2023-01-07 00:10:32 +0000

    I struggled to understand that the json is local

--- scripts/Index.js
+++ scripts/Index.js
@@ -1,3 +1,10 @@
-function OnLoad() {
-    $.getJSON()
-}
+$(function OnLoad() {
+    $.getJSON("Restraunts/Restraunts.json",function(data) {
+        window.alert("data activated")
+        var items = [];
+        $.each(data,function(key,val) {
+            items.push("<li>");
+            
+        })
+    })
+})
