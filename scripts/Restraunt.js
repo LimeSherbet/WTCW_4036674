ce76c1b89207dfe7b0418e6ae95c17493765b77c
John Gaskell <john.liam.gaskell@gmail.com>
2023-01-08 06:20:53 +0000

    Test

--- scripts/Restraunt.js
+++ scripts/Restraunt.js
@@ -3,9 +3,10 @@ $(function OnLoad() {
     var URLObject = new URL(urlString);
     var URLParmeter = URLObject.searchParams.get("Restraunt");
     window.alert(URLParmeter);
-    $.getJSON(URLParmeter + "/Restraunt.json",function(data) {
-        $.each(data,function(key,val) {  
-            
+    $.getJSON(URLParmeter + "/Restraunt.json", function (data) {
+        $.each(data, function (key, val) {
+            window.alert(key + val);
+
         })
     })
 })
