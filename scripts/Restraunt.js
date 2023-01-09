68e6f5e905ef646238f7bb38c295a2eee8a9d83e
John Gaskell <john.liam.gaskell@gmail.com>
2023-01-09 20:21:00 +0000

    Push

--- scripts/Restraunt.js
+++ scripts/Restraunt.js
@@ -6,5 +6,10 @@ $(function OnLoad() {
     $.getJSON("../"+ URLParmeter + "/Restaurant.json", function (data) {
         window.alert(data.About);
         $("#BlurbText").text(data.About);
+        $("#RestrauntLocation").attr("src",data.MapURL);
+        $("#VisitWebsite").attr("href", data.WebsiteLink);
+        $("#About").attr("href", window.location.href)
+        $("#FoodMenu").attr("href","../Templates/FoodMenu?Restaurant="+ URLParmeter)
+        $("#DrinkMenu").attr("href","../Templates/DrinkMenu?Restaurant="+ URLParmeter);
     })
 })
