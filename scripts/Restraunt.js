d1b47da1928a4402da620e5702839f94830f14c7
John Gaskell <john.liam.gaskell@gmail.com>
2023-01-09 20:06:20 +0000

    Push

--- scripts/Restraunt.js
+++ scripts/Restraunt.js
@@ -2,29 +2,9 @@ $(function OnLoad() {
     var urlString = document.location.href;
     var URLObject = new URL(urlString);
     var URLParmeter = URLObject.searchParams.get("Restaurant");
-    window.alert(URLParmeter);
+    
     $.getJSON("../"+ URLParmeter + "/Restaurant.json", function (data) {
-        $("#BlurbText").text = data.About;
-
-
-
-
-        $.each(data, function (key, val) {
-            switch (key) {
-                case "About":
-                    
-                    break;
-                case "WebsiteLink":
-                    break;
-                case "MapURL":
-                    break;
-                case "Socials":
-                    break;
-            
-                default:
-                    break;
-            }
-
-        })
+        window.alert(data.About);
+        $("#BlurbText").text(data.About);
     })
 })
